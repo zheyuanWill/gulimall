ed2069e02f182a3b9c2c25d75e5b71a7a72d5084
zheyuanWill <148622683+zheyuanWill@users.noreply.github.com>
2024-02-06 14:40:08 +1100

    网关gateway

---
+++ guligateway/src/main/java/com/example/guligateway/GuligatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.guligateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//排除和数据源有关配置因为网关gateway不需要
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GuligatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuligatewayApplication.class, args);
+    }
+
+}
