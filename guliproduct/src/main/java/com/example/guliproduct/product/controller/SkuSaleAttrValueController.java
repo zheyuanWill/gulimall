b514a8afbf2e74825e13d6135f3da55f5870dd3b
zheyuanWill <148622683+zheyuanWill@users.noreply.github.com>
2024-02-23 18:00:24 +1100

    购物车

--- guliproduct/src/main/java/com/example/guliproduct/product/controller/SkuSaleAttrValueController.java
+++ guliproduct/src/main/java/com/example/guliproduct/product/controller/SkuSaleAttrValueController.java
@@ -1,14 +1,11 @@
 package com.example.guliproduct.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.guliproduct.product.entity.SkuSaleAttrValueEntity;
 import com.example.guliproduct.product.service.SkuSaleAttrValueService;
@@ -39,6 +36,11 @@ public class SkuSaleAttrValueController {
         PageUtils page = skuSaleAttrValueService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+    @GetMapping(value = "/stringList/{skuId}")
+    public List<String> getSkuSaleAttrValues(@PathVariable("skuId") Long skuId) {
+        List<String> stringList = skuSaleAttrValueService.getSkuSaleAttrValuesAsStringList(skuId);
+        return stringList;
     }
 
 
